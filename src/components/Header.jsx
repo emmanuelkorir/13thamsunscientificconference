09972ce8f57c692dc1239eeca6e93f2698e1ee9d
Emmanuel Korir <emmanuelqkorir@gmail.com>
2023-04-03 17:10:31 +0300

    edit Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import logo from '../assets/amsun-logo.png';
 import './header.css';
 
@@ -8,15 +8,14 @@ function Header() {
     <header>
 <Navbar fixed="top" className="header__color" expand="lg">
 <Container>
-  <Navbar.Brand href="#home"><img src={logo} width="40" height="40" className="d-inline-block align-top" alt="amsun-logo" /></Navbar.Brand>
+  <Navbar.Brand href="/"><img src={logo} width="40" height="40" className="d-inline-block align-top" alt="amsun-logo" /></Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
     <Nav>
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">About</Nav.Link>
-      <Nav.Link href="#link">Speakers</Nav.Link>
-      <Nav.Link href="#link">Programme</Nav.Link>
-      <Nav.Link href="#link">FAQs</Nav.Link>
+      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/programme">Programme</Nav.Link>
+      <Nav.Link href="/speakers">Speakers</Nav.Link>
+            <Nav.Link href="/faqs">FAQs</Nav.Link>
     </Nav>
   </Navbar.Collapse>
 </Container>
